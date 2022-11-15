27b7990172e42b0a37452d6e1ff9113e167572b1
Kevin635 <experthead07@gmail.com>
2022-11-14 19:50:00 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+const cardImages = [
+
+{'src': "/img/card-1.jpg"},
+{'src': "/img/card-2.jpg"},
+{'src': "/img/card-3.jpg"},
+{'src': "/img/card-4.jpg"},
+{'src': "/img/card-5.jpg"},
+{'src': "/img/card-6.jpg"},
+{'src': "/img/card-7.jpg"},
+{'src': "/img/card-8.jpg"},
+
+]
+
+
 
 function App() {
+  const [cards,setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+// shuffle cards
+const shuffleCards = () =>{
+  const shuffledCards = [...cardImages,...cardImages]
+  .sort(()=> Math.random()-0.5)
+  .map((card)=>({...card,id:Math.random() }))
+
+  setCards(shuffledCards)
+  setTurns(0)
+
+}
+
+console.log(cards,turns)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card=>(
+          <div className="card" key={card.id}>
+            <div>
+              <img className='front' src={card.src} alt="card front" width="300" height="300" />
+              <img className='back' src="/img/cover_img2.jpg" alt="card back" width="300" height="300"/>
+            </div>
+          </div>
+        ))}
+      </div>
+
     </div>
   );
 }
